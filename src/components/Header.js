55a940c552aa7442195b442e095791bb878407cc
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-08-19 20:17:18 -0700

    change Header to functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/Header.css';
 import '../css/bootstrap.min.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <h1>Strain Explorer</h1>
-        <small>Powered by Cannabis Reports API</small>
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div className="header">
+      <h1>Strain Explorer</h1>
+      <small>Powered by Cannabis Reports API</small>
+    </div>
+  );
 }
 
 export default Header;
